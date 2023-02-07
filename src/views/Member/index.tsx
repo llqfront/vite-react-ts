1c9d5a80f7f604aa20f814626156c6c4fc7e5013
llqfront <llqfront@aliyun.com>
2023-02-07 20:16:34 +0800

    up

--- src/views/Member/index.tsx
+++ src/views/Member/index.tsx
@@ -5,14 +5,14 @@ import { counterSlice } from '@/features/actions';
 import Container from '@com/Container';
 import { pokemonApi } from '@/services/pokemon'
 const View: React.FC = (props) => {
-  const { data, error, isLoading } = pokemonApi.useGetPokemonByNameQuery('bulbasaur')
-  const count = useSelector((state: RootState) => state.counter.value)
-  const dispatch = useDispatch()
-  const { decrement, increment } = counterSlice.actions;
+  // const { data, error, isLoading } = pokemonApi.useGetPokemonByNameQuery('bulbasaur')
+  // const count = useSelector((state: RootState) => state.counter.value)
+  // const dispatch = useDispatch()
+  // const { decrement, increment } = counterSlice.actions;
   return (
     <React.Fragment>
       <Container>
-        <div>dsafMemberasdfa</div>
+        {/* <div>dsafMemberasdfa</div>
         <button
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
@@ -25,8 +25,8 @@ const View: React.FC = (props) => {
           onClick={() => dispatch(decrement())}
         >
           Decrement
-        </button>
-        {error ? (
+        </button>*/} 
+        {/*error ? (
         <>Oh no, there was an error</>
       ) : isLoading ? (
         <>Loading...</>
@@ -35,7 +35,7 @@ const View: React.FC = (props) => {
           <h3>{data?.species.name}</h3>
           <img src={data?.sprites.front_shiny} alt={data?.species.name} />
         </>
-      ) : null}
+      ) : null*/}
       </Container>
     </React.Fragment>
   )
